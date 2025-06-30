a39e4588f1f06fc240cb58f8062a53d4e3e920c4
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-06-30 09:56:27 +0600

    feat: route added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/bkash/pay', [BkashController::class, 'pay'])->name('bkash.pay');
+    Route::get('/bkash/callback', [BkashController::class, 'callback'])->name('bkash.callback');
+    Route::post('/bkash/create-payment', [BkashController::class, 'createPayment'])->name('bkash.create-payment');
+    Route::post('/bkash/execute-payment', [BkashController::class, 'executePayment'])->name('bkash.execute-payment');
+});
